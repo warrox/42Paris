1c4b04fc062583144c6e3c14a4a8d90e5738c0d2
warrox <91904446+warrox@users.noreply.github.com>
2024-02-29 15:52:00 +0100

    Add files via upload

--- psw/src/algo_sort.c
+++ psw/src/algo_sort.c
@@ -1,4 +1,5 @@
 #include "psw_lib.h"
+#include <stdio.h>
 
 int ft_is_sorted(t_data *data)
 {
@@ -15,6 +16,76 @@ int ft_is_sorted(t_data *data)
 	}
 	return(1);
 }
+void ft_sort_3(t_data *data)
+{
+	if(data->stack_a->nbr > data->stack_a->next->nbr && 
+		data->stack_a->nbr > data->stack_a->next->next->nbr)
+		{
+			ft_ra(data);
+			data->counter++;
+		}
+	else if (data->stack_a->nbr < data->stack_a->next->nbr && 
+	data->stack_a->nbr > data->stack_a->next->next->nbr)
+	{
+		ft_rra(data);
+		data->counter++;
+	}
+
+		if(data->stack_a->nbr > data->stack_a->next->nbr)
+		{
+			ft_sa(data);
+			data->counter++;
+		}
+}
+void	ft_sort_2(t_data *data)
+{
+	if(data->stack_a->nbr > data->stack_a->next->nbr)
+	{
+		ft_sa(data);
+		data->counter++;
+	}
+}
+void	ft_sort_4(t_data *data) // do not work with 1 4 3 2
+{
+	//[1][2][4][3]
+		//RA
+	//[2][4][3][1]
+		//RA
+	//[4][3][1][2]
+		//RA
+	//[3][1][2][4]
+	while(ft_is_sorted(data)!= 1) // not working
+	{
+		if(data->stack_a->nbr > data->stack_a->next->nbr)
+		{
+			ft_ra(data);
+			data->counter++;
+		}
+		else if(data->stack_a->nbr < data->stack_a->next->nbr &&
+		data->stack_a->nbr < data->stack_a->next->next->nbr)
+		{
+			ft_ra(data);
+			data->counter++;
+		}
+		if(data->stack_a->nbr > data->stack_a->next->nbr)
+		{
+			ft_sa(data);
+			data->counter++;
+		}
+		if(data->counter == 4)
+		{
+			ft_ra(data);
+			data->counter++;
+		}
+		printf("DATA COUNTER : %d\n",data->counter);
+	}
+}
+
+
+
+
+
+
 void ft_sort(t_data *data)
 {
 	// if 3, 4, >4
@@ -24,22 +95,12 @@ void ft_sort(t_data *data)
 	{
 		HERE IS THE MAIN LOOP TRYING TO CHECK IF STACK IS SORTED
 	}*/
+	if(ft_list_lenght(data->stack_a) == 2)
+		ft_sort_2(data);
 	if(ft_list_lenght(data->stack_a) == 3)
-	{
-			
-		if(data->stack_a->nbr > data->stack_a->next->nbr && 
-		data->stack_a->nbr > data->stack_a->next->next->nbr)
-		{
-			ft_ra(data);
-			data->counter++;
-		}
-
-		if(data->stack_a->nbr > data->stack_a->next->nbr)
-		{
-			ft_sa(data);
-			data->counter++;
-		}
-	}
-	if(ft_list_lenght(data->stack_a) > 3)
+		ft_sort_3(data);
+	if(ft_list_lenght(data->stack_a) == 4)
+		ft_sort_4(data);
+	if(ft_list_lenght(data->stack_a) > 4)
 		ft_pa(data);
 }
